a14ab7cd4558a35db7e8bfbb06ca7795ae1e6506
Dillon <82423961+DillonKyleDev@users.noreply.github.com>
2025-01-12 13:51:40 -0800

    Updated .gitignore

--- FlatEngine-Core/Source/Model.cpp
+++ FlatEngine-Core/Source/Model.cpp
@@ -195,7 +195,7 @@ namespace FlatEngine
         UniformBufferObject ubo{};
         ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f)); // Creates a vec4 that is used to matrix multiply by the positions of each vertex.
         ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-        ubo.proj = glm::perspective(glm::radians(45.0f), winSystem.GetExtent().width / (float)winSystem.GetExtent().height, 0.1f, 10.0f);
+        ubo.proj = glm::perspective(glm::radians(90.0f), winSystem.GetExtent().width / (float)winSystem.GetExtent().height, 0.1f, 10.0f);
         ubo.proj[1][1] *= -1;
 
         memcpy(m_uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
